49d422d8716c7a83285431586c0ba23a7603cb05
Федор-ПК\Федор <curafedor@gmail.com>
2021-01-09 13:20:35 +0200

    HTarray2

---
+++ HTarray2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HTarray2
+{
+    class Program
+    {
+        static int IndexOf(int[] array, int value)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == value )
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 3, 4, 5, 6, 7, 8, 9 };
+
+            int result = IndexOf(myArray, 6);
+
+            Console.WriteLine(result);
+        }
+    }
+}
